eb0a0165448445f37f72234d1559d99723dbcf16
Welliton Paiva <wellitonpaiva@gmail.com>
2024-12-01 12:46:18 +0000

    first day done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,27 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+import kotlin.math.abs
+
+fun String.resolve(): List<Pair<Int, Int>> =
+    this.lines().map {
+        val numbers = it.split("   ")
+        numbers[0].toInt() to numbers[1].toInt()
+    }
+
+fun List<Pair<Int, Int>>.findDistances(): Int {
+    val firstList = mapIndexed { pos, dis -> (pos to dis.first) }.sortedBy { pair -> pair.second }
+    val secondList = mapIndexed { pos, dis -> (pos to dis.second) }.sortedBy { pair -> pair.second }
+
+    return firstList.mapIndexed { num, pos -> abs(pos.second - secondList[num].second) }.sum()
+}
+
+fun List<Pair<Int, Int>>.findSimilarities(): Int {
+    return map { it.first }.sumOf { find -> find * map { it.second }.filter { it == find }.size }
+
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val pairs = Path("src/Day1_input.txt").readText().trim().resolve()
+    println("Total Distance is: ${pairs.findDistances()}")
+    println("Similarities sum are: ${pairs.findSimilarities()}")
 }
